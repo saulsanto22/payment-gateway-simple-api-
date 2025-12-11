cd5129c4e38e1d2504f2c0137855067866bbe6c9
saulsanto22 <saul.santoa@gmail.com>
2025-12-11 15:51:43 +0700

    Add yml gitlab

--- app/Console/Commands/ReminderUnpaidOrder.php
+++ app/Console/Commands/ReminderUnpaidOrder.php
@@ -32,6 +32,7 @@ class ReminderUnpaidOrder extends Command
     // buat depedensi injection untuk orderRepository
     public function __construct(OrderRepository $orderRepository)
     {
+        parent::__construct();
         $this->orderRepository = $orderRepository;
 
     }
@@ -48,9 +49,9 @@ class ReminderUnpaidOrder extends Command
             \Log::info($order->user->email);
 
         }
-        \Log::info('Reminder command triggered at '.now());
+        \Log::info('Reminder command triggered at ' . now());
 
-        $this->info('Reminder emails dispatched for '.count($orders).' unpaid orders.');
+        $this->info('Reminder emails dispatched for ' . count($orders) . ' unpaid orders.');
 
     }
 }
